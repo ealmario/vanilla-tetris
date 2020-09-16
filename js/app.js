33658727e58f8717062b9de1a364ddec8cac7c70
ealmario <ee.almario@gmail.com>
2020-09-16 19:16:09 +0800

    Add basic setup

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   //code here
+  const gameContainer = document.getElementById('gameContainer');
+  for (let i = 0; i < 200; i++) {
+    const square = document.createElement("div");
+    gameContainer.appendChild(square);
+  }
 });
