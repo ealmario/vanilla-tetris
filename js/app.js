2fa8522ab08332fda7617d57d242dffe9bc6f596
ealmario <ee.almario@gmail.com>
2020-09-16 21:53:48 +0800

    Add tetrominoes

--- js/app.js
+++ js/app.js
@@ -1,8 +1,53 @@
 document.addEventListener('DOMContentLoaded', () => {
-  //code here
-  const gameContainer = document.getElementById('gameContainer');
+  const gameContainer = document.getElementById('game-container');
+  const scoreDisplay = document.getElementById('score');
+  const startBtn = document.getElementById('start-btn')
+  const width = 10;
+
+  // Create grid divs
   for (let i = 0; i < 200; i++) {
     const square = document.createElement("div");
     gameContainer.appendChild(square);
   }
+
+  // Get all the squares inside the game container
+  let squares = Array.from(document.querySelectorAll('.game-container div'));
+
+  // Tetrominoes
+  const lTetramino = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+  ]
+
+  const zTetromino = [
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1],
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1]
+  ]
+
+  const tTetromino = [
+    [1,width,width+1,width+2],
+    [1,width+1,width+2,width*2+1],
+    [width,width+1,width+2,width*2+1],
+    [1,width,width+1,width*2+1]
+  ]
+
+  const oTetromino = [
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1]
+  ]
+
+  const iTetromino = [
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3],
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3]
+  ]
+
+  const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetrominp, iTetromino];
 });
